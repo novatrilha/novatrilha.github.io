51898eaacc9e037a188feb0e511c0270deca928d
Nova Trilha <estamosnatrilha@gmail.com>
2022-08-22 22:00:34 -0300

    Update static files

---
+++ _app/immutable/chunks/1-cc210737.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/error.svelte-ad032ad8.js";import"./index-29b12e8d.js";import"./stores-59e4bc29.js";import"./singletons-21087335.js";export{e as component};
--- _app/immutable/chunks/2-4e05d935.js
+++
@@ -1,1 +0,0 @@
-import{default as m}from"../components/pages/_page.svelte-d057780b.js";import"./index-29b12e8d.js";export{m as component};
