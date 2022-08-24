ce5cb935682b51407cf29c32ba2612529055dfab
Nova Trilha <estamosnatrilha@gmail.com>
2022-08-24 01:57:22 -0300

    Update static files

---
+++ _app/immutable/chunks/0-440d6f54.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_layout.svelte-25dbe19a.js";import"./singletons-2b75d96c.js";import"./navigation-873b58b5.js";import"./stores-6f765d97.js";export{e as component};
---
+++ _app/immutable/chunks/navigation-873b58b5.js
@@ -0,0 +1,1 @@
+import{T as e}from"./singletons-2b75d96c.js";e.disable_scroll_handling;const a=e.goto;e.invalidate;e.prefetch;e.prefetch_routes;e.before_navigate;e.after_navigate;export{a as g};
